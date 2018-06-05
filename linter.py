d8d29f85886c95c2f3309cfd8ceb41daf2ad355a
William Woodruff <william@yossarian.net>
2018-06-05 01:15:05 -0400

    linter: Remove unused fields
    SublimeLinter apparently no longer uses these.

--- linter.py
+++ linter.py
@@ -17,9 +17,6 @@ class GitLintCommit(Linter):
     """Provides an interface to git-lint-commit."""
     selector = 'text.git.commit'
     cmd = 'git-lint-commit -'
-    version_args = '--version'
-    version_re = r'git-lint-commit version (?P<version>\d+\.\d+\.\d+)\.'
-    version_requirement = '>= 1.0.4'
     regex = r'^(?:(?P<warning>W)|(?P<error>E)): Line (?P<line>\d+): (?P<message>.+)'
     error_stream = util.STREAM_BOTH
     line_col_base = (1, 1)
